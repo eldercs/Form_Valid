1fba906acea4f54ad7d340bc41bbbe04ec4411d7
Denis Druchevky <47366844+eldercs@users.noreply.github.com>
2022-04-01 17:50:58 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import Vuelidate from 'vuelidate'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+Vue.config.devtools = true
+
 Vue.use(Vuelidate)
 new Vue({
   render: h => h(App),
